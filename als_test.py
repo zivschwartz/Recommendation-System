cba61e24b961d4a605d0f0c23837597804937ca9
Micaela Flores <mrf444@nyu.edu>
2019-05-13 16:34:23 -0400

    sans hyperparam tuning

---
+++ als_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# We need sys to get the command line arguments
+import sys
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.ml.feature import StringIndexer
+from pyspark.sql import functions as F
+from pyspark.mllib.evaluation import RankingMetrics
+from pyspark.sql.functions import col
+
+def main(spark, model_file,data_file,user_file,track_file):
+   
+    #load ALS model
+    als_model = ALSModel.load(model_file)
+    user_indexer = StringIndexer.load(user_file)
+    track_indexer = StringIndexer.load(track_file)
+
+    #read in test data as parquet
+    df_test = spark.read.parquet(data_file)
+    pipeline = Pipeline(stages=[user_indexer, track_indexer])
+    mapping = pipeline.fit(df_test)
+    df_test = mapping.transform(df_test)
+
+    ########### PERFORM RANKING METRICS ##########
+    #create user actual items dataframe
+    actual_recs = df_test.groupBy('user_idx')\
+                    .agg(F.collect_list('track_idx').alias('track_idx'))
+     
+    #create user predicted items dataframe
+    user_subset = df_test.select('user_idx').distinct()
+    pred_recs = als_model.recommendForUserSubset(user_subset,10)
+    pred_recs = pred_recs.select('user_idx',\
+                    col('recommendations.track_idx').alias('track_idx'))
+
+    #create user item RDD & join on users 
+    perUserItemsRDD = pred_recs\
+                        .join(actual_recs, on='user_idx').rdd\
+                        .map(lambda row: (row[1], row[2]))
+    
+    rankingMetrics = RankingMetrics(perUserItemsRDD)
+
+    #print results to the console
+    print("Ranking Metrics MAP: {}",rankingMetrics.meanAveragePrecision)
+
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('als_test').getOrCreate()
+
+    # And the location to store the trained model
+    model_file = sys.argv[1]
+
+    # Get the filename from the command line
+    data_file = sys.argv[2]
+
+    # Read String Indexers
+    user_file = sys.argv[3]
+    track_file = sys.argv[4]
+
+    # Call our main routine
+    main(spark, model_file, data_file,user_file,track_file)
---
+++ als_train.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# We need sys to get the command line arguments
+import sys
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.recommendation import ALS
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.sql import functions as F
+from pyspark.sql.functions import col
+from pyspark.mllib.evaluation import RankingMetrics
+
+def main(spark, data_file, model_file,user_file,track_file):
+    df = spark.read.parquet(data_file).sample(0.01)
+    
+    user_indexer = StringIndexer(inputCol="user_id", outputCol="user_idx",\
+                        handleInvalid='keep')
+    track_indexer = StringIndexer(inputCol="track_id", outputCol="track_idx",\
+                        handleInvalid='keep')
+
+    pipeline = Pipeline(stages=[user_indexer, track_indexer])
+    mapping = pipeline.fit(df)
+    df = mapping.transform(df)
+    
+    #create + fit an ALS model
+    als = ALS(maxIter=5, regParam=0.01, implicitPrefs=True,\
+                        userCol="user_idx", itemCol="track_idx")
+    als_model = als.fit(df)
+    
+    #save trained ALS model
+    als_model.write().overwrite().save(model_file)
+    print("Model sucessfully saved to HFS")
+    
+    #save string indexers
+    user_indexer.write().overwrite().save(user_file)
+    track_indexer.write().overwrite().save(track_file)
+    print("String Indexers sucessfully saved to HFS")
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName('als_train').getOrCreate()
+    data_file = sys.argv[1]
+    model_file = sys.argv[2]
+    user_file = sys.argv[3]
+    track_file = sys.argv[4]
+
+    main(spark,data_file,model_file,user_index,track_index)
